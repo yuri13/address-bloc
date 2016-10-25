3e0233f93b80a15d85029021bbac6bfcd83b9bf0
Yuri Reyes <reyestomas13@gmail.com>
2016-10-25 13:58:21 -0700

    Checkpoint 6 complete

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,9 @@
-puts "Welcome to Address Bloc!"
+require_relative 'controllers/menu_controller'
+
+# #4
+menu = MenuController.new
+# #5
+system "clear"
+puts "Welcome to AddressBloc!"
+# #6
+menu.main_menu
